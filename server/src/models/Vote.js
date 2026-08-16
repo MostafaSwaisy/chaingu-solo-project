9da75210922ccccb2001e4f392dd29e05cd21e84
Mostafa Swaisy <motafa28king@gmail.com>
2026-08-16 11:40:17 +0300

    Add Poll/Vote models and pollService with vote-once enforcement

---
+++ server/src/models/Vote.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const voteSchema = new mongoose.Schema(
+  {
+    pollId: { type: mongoose.Schema.Types.ObjectId, ref: 'Poll', required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    optionIndex: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+voteSchema.index({ pollId: 1, userId: 1 }, { unique: true });
+
+export const Vote = mongoose.model('Vote', voteSchema);
